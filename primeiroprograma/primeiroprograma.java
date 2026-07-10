fa37ac6ed8a857a68c0343a6da950b1d8f886ad1
Francisco Euzb <2013talgia@gmail.com>
2026-07-10 14:40:18 -0300

    advancing

---
+++ primeiroprograma/primeiroprograma.java
@@ -0,0 +1,17 @@
+
+//para iniciar um programa, devemos criar o cabeçalho dele, ou seja, uma classe (lembrando: isso é um programa)
+//tudo oque estiver entre chaves, é um bloco, em muitas linguagens + o java
+//criar um pacote (não necessário) para criar uma hierarquia
+package primeiroprograma; 
+// um pacote, serve para acharmos o arquivo com o nome da pasta em que ele está, como pode ser visto na barra lateral
+public class primeiroprograma {
+//criar um método, método principal, main
+public static void main(String[] args) {
+//agora escrever um código que mostra algo na tela, ou no caso palavras
+System.out.print("Oie, meu primeiro programa em Java!");
+//certifique-se de fechar com ";" quando for terminar o print
+//se vc usar print com ln no final, qualquer argumento do terminal fica em baixo, ou qualquer outro print
+
+}
+
+}
